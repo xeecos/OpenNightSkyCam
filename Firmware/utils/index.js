27bacd866306b7f44d8ee609993120e772c98570
虎子哥 <flashindream@gmail.com>
2023-03-28 10:14:26 +0800

    optimize

---
+++ Firmware/utils/index.js
@@ -0,0 +1,150 @@
+
+document.getElementById("defaultOpen").click();
+document.getElementById("exp-input").value = document.getElementById("exp-range").value = localStorage.getItem("exposure", 1);
+document.getElementById("r-gain-input").value = document.getElementById("r-gain-range").value = localStorage.getItem("r-gain", 1);
+document.getElementById("g-gain-input").value = document.getElementById("g-gain-range").value = localStorage.getItem("g-gain", 1);
+document.getElementById("b-gain-input").value = document.getElementById("b-gain-range").value = localStorage.getItem("b-gain", 1);
+document.getElementById("interval-input").value = document.getElementById("interval-range").value = localStorage.getItem("interval", 1);
+document.getElementById("frames-input").value = document.getElementById("frames-range").value = localStorage.getItem("frames", 1);
+document.getElementById("progress-input").value = " 0 / " + document.getElementById("frames-input").value;
+document.getElementById("video-length-input").value = (document.getElementById("frames-input").value / 30).toFixed(0);
+document.getElementById("exp-input").oninput = document.getElementById("exp-range").oninput = function () {
+    document.getElementById("exp-input").value = this.value;
+    document.getElementById("exp-range").value = this.value;
+    localStorage.setItem("exposure", this.value);
+}
+document.getElementById("r-gain-input").oninput = document.getElementById("r-gain-range").oninput = function () {
+    document.getElementById("r-gain-input").value = this.value;
+    document.getElementById("r-gain-range").value = this.value;
+    localStorage.setItem("r-gain", this.value);
+}
+document.getElementById("g-gain-input").oninput = document.getElementById("g-gain-range").oninput = function () {
+    document.getElementById("g-gain-input").value = this.value;
+    document.getElementById("g-gain-range").value = this.value;
+    localStorage.setItem("g-gain", this.value);
+}
+document.getElementById("b-gain-input").oninput = document.getElementById("b-gain-range").oninput = function () {
+    document.getElementById("b-gain-input").value = this.value;
+    document.getElementById("b-gain-range").value = this.value;
+    localStorage.setItem("b-gain", this.value);
+}
+
+document.getElementById("interval-input").oninput = document.getElementById("interval-range").oninput = function () {
+    document.getElementById("interval-input").value = this.value;
+    document.getElementById("interval-range").value = this.value;
+    localStorage.setItem("interval", this.value);
+}
+document.getElementById("frames-input").oninput = document.getElementById("frames-range").oninput = function () {
+    document.getElementById("frames-input").value = this.value;
+    document.getElementById("frames-range").value = this.value;
+    localStorage.setItem("frames", this.value);
+
+    document.getElementById("progress-input").value = " 0 / " + document.getElementById("frames-input").value;
+    document.getElementById("video-length-input").value = (document.getElementById("frames-input").value / 30).toFixed(0);
+}
+function openPage(evt, pageName) {
+    let i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(pageName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+let waitTime = 1000;
+function request(url) {
+    return new Promise(resolve => {
+        document.getElementById('appLoading').style.display = 'block';
+        let oReq = new XMLHttpRequest();
+        oReq.addEventListener("load", function () {
+            document.getElementById('appLoading').style.display = 'none'
+            resolve(this.responseText);
+        });
+        oReq.open("GET", url);
+        oReq.send();
+    });
+}
+async function onShot() {
+    document.getElementById('appLoading').style.display = 'block';
+    await onExposure();
+    request('/capture/shot').then(res => {
+        let obj = JSON.parse(res);
+        setTimeout(() => {
+            document.getElementById('appLoading').style.display = 'none'
+            onReload();
+        }, obj.time * 1.0 + waitTime);
+    });
+}
+function onReload() {
+    document.getElementById('preview-img').src = '/preview/latest.jpg?time=' + Date.now();
+}
+function onExposure() {
+    return new Promise(resolve => {
+        let exp = document.getElementById('exp-input').value * 1000;
+        let r_gain = document.getElementById('r-gain-input').value * 1000;
+        let g_gain = document.getElementById('g-gain-input').value * 1000;
+        let b_gain = document.getElementById('b-gain-input').value * 1000;
+        request(`/capture/set?coarse=${exp}&fine=${1}&r_gain=${r_gain}&gr_gain=${g_gain}&gb_gain=${g_gain}&b_gain=${b_gain}`).then(res => {
+            resolve(JSON.parse(res))
+        });
+    })
+}
+function onAddTask() {
+    return new Promise(resolve => {
+        let exp = document.getElementById('exp-input').value * 1000;
+        let r_gain = document.getElementById('r-gain-input').value * 1000;
+        let g_gain = document.getElementById('g-gain-input').value * 1000;
+        let b_gain = document.getElementById('b-gain-input').value * 1000;
+        request(`/task/add?coarse=${exp}&fine=${1}&r_gain=${r_gain}&gr_gain=${g_gain}&gb_gain=${g_gain}&b_gain=${b_gain}`).then(res => {
+            resolve(JSON.parse(res))
+        });
+    })
+}
+function onStartTask() {
+    return new Promise(resolve => {
+        let exp = document.getElementById('exp-input').value * 1000;
+        let r_gain = document.getElementById('r-gain-input').value * 1000;
+        let g_gain = document.getElementById('g-gain-input').value * 1000;
+        let b_gain = document.getElementById('b-gain-input').value * 1000;
+        request(`/task/start`).then(res => {
+            resolve(JSON.parse(res))
+        });
+    })
+}
+function onStopTask() {
+    return new Promise(resolve => {
+        let exp = document.getElementById('exp-input').value * 1000;
+        let r_gain = document.getElementById('r-gain-input').value * 1000;
+        let g_gain = document.getElementById('g-gain-input').value * 1000;
+        let b_gain = document.getElementById('b-gain-input').value * 1000;
+        request(`/task/stop`).then(res => {
+            resolve(JSON.parse(res))
+        });
+    })
+}
+function getTaskStatus() {
+    return new Promise(resolve => {
+        request(`/task/status`).then(res => {
+            resolve(JSON.parse(res))
+        });
+    })
+}
+
+function syncTime() {
+    return new Promise(resolve => {
+        request(`/time/set?time=${Date.now()}`).then(res => {
+            resolve(JSON.parse(res))
+        });
+    })
+}
+function getDirectoryList(path) {
+    return new Promise(resolve => {
+        request(`/${path}`).then(res => {
+            resolve(JSON.parse(res))
+        });
+    })
+}
