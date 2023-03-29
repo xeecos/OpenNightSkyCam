d8ac44726a895c2722f4132daf4172ca827a68c8
虎子哥 <flashindream@gmail.com>
2023-03-29 10:26:00 +0800

    bugfix

--- Firmware/utils/api.js
+++ Firmware/utils/api.js
@@ -1,11 +1,11 @@
 
 document.getElementById("defaultOpen").click();
-document.getElementById("exp-input").value = document.getElementById("exp-range").value = localStorage.getItem("exposure", 1);
-document.getElementById("r-gain-input").value = document.getElementById("r-gain-range").value = localStorage.getItem("r-gain", 1);
-document.getElementById("g-gain-input").value = document.getElementById("g-gain-range").value = localStorage.getItem("g-gain", 1);
-document.getElementById("b-gain-input").value = document.getElementById("b-gain-range").value = localStorage.getItem("b-gain", 1);
-document.getElementById("interval-input").value = document.getElementById("interval-range").value = localStorage.getItem("interval", 1);
-document.getElementById("frames-input").value = document.getElementById("frames-range").value = localStorage.getItem("frames", 1);
+document.getElementById("exp-input").value = document.getElementById("exp-range").value = localStorage.getItem("exposure")||1;
+document.getElementById("r-gain-input").value = document.getElementById("r-gain-range").value = localStorage.getItem("r-gain")||1;
+document.getElementById("g-gain-input").value = document.getElementById("g-gain-range").value = localStorage.getItem("g-gain")||1;
+document.getElementById("b-gain-input").value = document.getElementById("b-gain-range").value = localStorage.getItem("b-gain")||1;
+document.getElementById("interval-input").value = document.getElementById("interval-range").value = localStorage.getItem("interval")||1;
+document.getElementById("frames-input").value = document.getElementById("frames-range").value = localStorage.getItem("frames")||1;
 document.getElementById("progress-input").value = " 0 / " + document.getElementById("frames-input").value;
 document.getElementById("video-length-input").value = (document.getElementById("frames-input").value / 30).toFixed(0);
 document.getElementById("exp-input").oninput = document.getElementById("exp-range").oninput = function () {
